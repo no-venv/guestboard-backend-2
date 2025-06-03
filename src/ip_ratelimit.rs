39e9baa24ee5c8474e2d775923bdd6d84d27957e
no-venv <>
2025-06-02 19:54:58 -0600

    ratelimit security fix 1.2

--- src/ip_ratelimit.rs
+++ src/ip_ratelimit.rs
@@ -10,7 +10,10 @@ impl IpRatelimit {
         // let queue: Vec<_> = Vec::new();
         for ip in self.ip_list.clone().into_keys() {
             let ip = ip.to_string();
-            self.remove_ratelimit(&ip);
+            let result = self.remove_ratelimit(&ip);
+            if result{
+                println!("removed stale ip");
+            }
         }
     }
     pub fn ratelimit(&mut self, ip: String) {
@@ -31,7 +34,7 @@ impl IpRatelimit {
             Some(time) => {
                 let now = SystemTime::now();
                 let distance = now.duration_since(*time).expect("wtf ip time error");
-                let time_left = distance.as_secs().checked_sub(RATELIMIT_SECS);
+                let time_left = RATELIMIT_SECS.checked_sub(distance.as_secs());
                 if time_left.is_some() {
                     time_left.unwrap()
                 } else {
